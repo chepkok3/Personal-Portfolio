90850040bd0a145df332ac6144d2b78f8e93ac5a
Stanley <78428936+chepkok3@users.noreply.github.com>
2023-01-02 21:53:05 +0300

    Fix linter errors

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const hamburger = document.querySelector(".hamburger-icon");
-const navMenu = document.querySelector(".nav-menu");
-const navItem = document.querySelectorAll(".nav-item");
-const cancelMenu = document.querySelector(".menu-cancel");
+const hamburger = document.querySelector('.hamburger-icon');
+const navMenu = document.querySelector('.nav-menu');
+const navItem = document.querySelectorAll('.nav-item');
+const cancelMenu = document.querySelector('.menu-cancel');
 
-hamburger.addEventListener("click", () => {
-  navMenu.style.left = "0%";
+hamburger.addEventListener('click', () => {
+  navMenu.style.left = '0%';
 });
 
-cancelMenu.addEventListener("click", () => {
-  navMenu.style.left = "-100%";
+cancelMenu.addEventListener('click', () => {
+  navMenu.style.left = '-100%';
 });
 
-navItem.forEach(function(list) {
-  list.addEventListener('click', function() {
-    navMenu.style.left = "-100%";
-  })
-})
+navItem.forEach((list) => {
+  list.addEventListener('click', () => {
+    navMenu.style.left = '-100%';
+  });
+});
